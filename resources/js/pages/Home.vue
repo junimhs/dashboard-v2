28c1ae60244f864ca8fb3a172a875096e09abbc0
Luis Henrique <junimhs10@gmail.com>
2021-06-12 11:20:28 -0300

    Install inertiajs and tailwindcss

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="text-center text-red-400">Home</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
