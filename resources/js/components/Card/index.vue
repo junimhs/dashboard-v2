5f7b9499a80702afe5244796f40b361dd10f8b35
Luis Henrique <junimhs10@gmail.com>
2021-06-18 20:13:43 -0300

    Create component card

---
+++ resources/js/components/Card/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+        <div class="bg-white rounded-lg shadow px-4 py-2 sm:px-6">
+            <slot />
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/pages/users/index.vue
+++ resources/js/pages/users/index.vue
@@ -11,75 +11,76 @@
             </header>
         </div>
         <main class="-mt-32">
-            <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+            <card>
                 <!-- Replace with your content -->
-                <div class="bg-white rounded-lg shadow px-4 py-2 sm:px-6">
-                    <div class="border-b border-gray-200 py-3 flex justify-between">
-                        <div>
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Filtrar usuarios
-                            </h3>
-                            <p class="mt-1 text-sm text-gray-500">
-                                Preencha os campos abaixo para filtragem de usuarios
-                            </p>
-                        </div>
-                        <div class="flex items-center">
-                            <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
-                            <button @click="showTrashed" type="button"
-                                    class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                    :class="{'bg-indigo-600': isTrashed}"
-                                    role="switch" aria-checked="false" aria-labelledby="annual-billing-label">
-                                <span class="sr-only">Use setting</span>
-                                <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-                                <span aria-hidden="true"
-                                      class="translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                                      :class="{'translate-x-5': isTrashed}"
-                                ></span>
-                            </button>
-                            <span class="ml-3" id="annual-billing-label">
+                <div class="border-b border-gray-200 py-3 flex justify-between">
+                    <div>
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            Filtrar usuarios
+                        </h3>
+                        <p class="mt-1 text-sm text-gray-500">
+                            Preencha os campos abaixo para filtragem de usuarios
+                        </p>
+                    </div>
+                    <div class="flex items-center">
+                        <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
+                        <button @click="showTrashed" type="button"
+                                class="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                :class="{'bg-indigo-600': isTrashed}"
+                                role="switch" aria-checked="false" aria-labelledby="annual-billing-label">
+                            <span class="sr-only">Use setting</span>
+                            <!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
+                            <span aria-hidden="true"
+                                  class="translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+                                  :class="{'translate-x-5': isTrashed}"
+                            ></span>
+                        </button>
+                        <span class="ml-3" id="annual-billing-label">
                                 <span class="text-sm font-medium text-gray-900">{{ isTrashed ? 'Usuarios excluidos' : 'Usuarios ativos' }}</span>
                             </span>
-                        </div>
-                    </div>
-                    <div class="mt-4">
-                        <div class="flex flex-col">
-                            <div class="grid grid-cols-4 gap-4">
-                                <text-input
-                                    label="Id"
-                                    name="id"
-                                    placeholder="Id"
-                                    icon="KeyIcon"
-                                    v-model="params.id"
-                                />
-                            </div>
-                            <div class="mt-4 grid grid-cols-2 gap-4">
-                                <text-input
-                                    label="Nome"
-                                    name="name"
-                                    placeholder="Nome"
-                                    icon="UserIcon"
-                                    v-model="params.name"
-
-                                />
-                                <text-input
-                                    label="E-mail"
-                                    name="email"
-                                    type="email"
-                                    placeholder="E-mail"
-                                    icon="MailIcon"
-                                    v-model="params.email"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="py-4 flex justify-end">
-                        <loading-button title="Filtrar usuario" type="button" icon="SearchIcon" @click="filterUser"
-                                        :loading="loading"/>
-
-                        <loading-button class="ml-2" title="Resetar filtro" icon="CloseIcon" @click="resetFilter"
-                                        v-if="isFilter"/>
                     </div>
                 </div>
+                <div class="mt-4">
+                    <div class="flex flex-col">
+                        <div class="grid grid-cols-4 gap-4">
+                            <text-input
+                                label="Id"
+                                name="id"
+                                placeholder="Id"
+                                icon="KeyIcon"
+                                v-model="params.id"
+                            />
+                        </div>
+                        <div class="mt-4 grid grid-cols-2 gap-4">
+                            <text-input
+                                label="Nome"
+                                name="name"
+                                placeholder="Nome"
+                                icon="UserIcon"
+                                v-model="params.name"
+
+                            />
+                            <text-input
+                                label="E-mail"
+                                name="email"
+                                type="email"
+                                placeholder="E-mail"
+                                icon="MailIcon"
+                                v-model="params.email"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="py-4 flex justify-end">
+                    <loading-button title="Filtrar usuario" type="button" icon="SearchIcon" @click="filterUser"
+                                    :loading="loading"/>
+
+                    <loading-button class="ml-2" title="Resetar filtro" icon="CloseIcon" @click="resetFilter"
+                                    v-if="isFilter"/>
+                </div>
+                <!-- /End replace -->
+            </card>
+            <card>
                 <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6 mt-4">
                     <div class="bg-white shadow overflow-hidden sm:rounded-md">
                         <ul class="divide-y divide-gray-200">
@@ -129,8 +130,7 @@
                         <pagination :data="users"/>
                     </div>
                 </div>
-                <!-- /End replace -->
-            </div>
+            </card>
         </main>
     </base-layout>
 </template>
@@ -141,10 +141,11 @@ import InertiaButton from "../../components/InertiaButton";
 import TextInput from "../../components/TextInput";
 import LoadingButton from "../../components/LoadingButton";
 import Pagination from "../../components/Pagination";
+import Card from "../../components/Card";
 
 export default {
     name: 'User.index',
-    components: {Pagination, LoadingButton, TextInput, InertiaButton, BaseLayout},
+    components: {Card, Pagination, LoadingButton, TextInput, InertiaButton, BaseLayout},
     props: {
         users: Object,
         filters: Object | Array
